a5105e419fe24e43fa3e58e99c49db7e98627def
Ray Xia <77950209+xiatiantian24@users.noreply.github.com>
2024-04-24 08:18:09 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,60 +1,271 @@
-var Split = function() {
-    this.$t = document.querySelector(".split");
-    this.gridX = 8;
-    this.gridY = 6;
-    this.w = this.$t.offsetWidth;
-    this.h = this.$t.offsetHeight;
-    this.img = this.$t.querySelector("img").getAttribute("src");
-    this.delay = 0.05;
+var loadSplit;
 
-    this.create = function() {
-        var self = this;
-        while (this.$t.firstChild) {
-            this.$t.removeChild(this.$t.firstChild);
-        }
+var menuX, menuY;
+var menu = document.getElementById("menu");
+var menuText = document.getElementById("menu-text");
 
-        for (var x = 0; x < this.gridX; x++) {
-            for (var y = 0; y < this.gridY; y++) {
-                var width = (this.w / this.gridX * 101 / this.w) + "%",
-                    height = (this.h / this.gridY * 101 / this.h) + "%",
-                    top = (this.h / this.gridY * y * 100 / this.h) + "%",
-                    left = (this.w / this.gridX * x * 100 / this.w) + "%",
-                    bgPosX = -(this.w / this.gridX * x) + "px",
-                    bgPosY = -(this.h / this.gridY * y) + "px";
+const powerpointButton = document.getElementById("powerpoint-button");
+const wordButton = document.getElementById("word-button");
+const excelButton = document.getElementById("excel-button");
 
-                var div = document.createElement("div");
-                div.style.top = top;
-                div.style.left = left;
-                div.style.width = width;
-                div.style.height = height;
-                div.style.backgroundImage = "url(" + this.img + ")";
-                div.style.backgroundPosition = bgPosX + " " + bgPosY;
-                div.style.backgroundSize = this.w + "px";
-                div.style.transitionDelay = x * this.delay + y * this.delay + "s";
+const prevBlockButton = document.getElementById("prev-block");
+const prevRowButton = document.getElementById("prev-row");
+const prevColumnButton = document.getElementById("prev-column");
+const prevCanvasButton = document.getElementById("prev-canvas");
+const nextBlockButton = document.getElementById("next-block");
+const nextRowButton = document.getElementById("next-row");
+const nextColumnButton = document.getElementById("next-column");
+const nextCanvasButton = document.getElementById("next-canvas");
 
-                this.$t.appendChild(div);
-            }
-        }
-    };
+var Split = function () {
+  this.splitElement = document.querySelector(".split");
+  this.gridX = 16;
+  this.gridY = 10;
+  this.width = this.splitElement.offsetWidth;
+  this.height = this.splitElement.offsetHeight;
+  this.imageSrc = this.splitElement.querySelector("img").getAttribute("src");
+  this.delay = 0;
+  this.currentActiveDiv = null;
 
-    this.create();
+  this.divs = {};
 
+  // Define text for each block
+  this.textMappings = {
+    // div_0_0: "This is an apple",
+    // div_1_0: "My name is Bob",
+    // Add more mappings as needed...
+  };
+
+  this.createDivs = function () {
     var self = this;
-    this.$t.addEventListener("click", function() {
-        self.$t.classList.toggle("active");
+    while (this.splitElement.firstChild) {
+      this.splitElement.removeChild(this.splitElement.firstChild);
+    }
+
+    for (var x = 0; x < this.gridX; x++) {
+      for (var y = 0; y < this.gridY; y++) {
+        var width = ((this.width / this.gridX) * 101) / this.width + "%",
+          height = ((this.height / this.gridY) * 101) / this.height + "%",
+          top = ((this.height / this.gridY) * y * 100) / this.height + "%",
+          left = ((this.width / this.gridX) * x * 100) / this.width + "%",
+          bgPosX = -((this.width / this.gridX) * x) + "px",
+          bgPosY = -((this.height / this.gridY) * y) + "px";
+
+        var div = document.createElement("div");
+        div.style.top = top;
+        div.style.left = left;
+        div.style.width = width;
+        div.style.height = height;
+        div.style.backgroundImage = "url(" + this.imageSrc + ")";
+        div.style.backgroundPosition = bgPosX + " " + bgPosY;
+        div.style.backgroundSize = this.width + "px";
+        div.style.transitionDelay = x * this.delay + y * this.delay + "s";
+
+        // Set ID for each div in the format "div_x_y"
+        var divName = "div_" + x + "_" + y;
+        div.setAttribute("id", divName);
+
+        this.divs[divName] = div;
+
+        this.splitElement.appendChild(div);
+
+        div.addEventListener("click", function () {
+          self.toggleActive(this);
+        });
+      }
+    }
+  };
+
+  this.createDivs();
+
+  this.toggleActive = function (div) {
+    if (this.currentActiveDiv !== null && this.currentActiveDiv !== div) {
+      this.currentActiveDiv.classList.remove("active");
+    }
+    this.currentActiveDiv = div;
+    div.classList.toggle("active");
+
+    var rect = div.getBoundingClientRect();
+    menuX = rect.left + rect.width;
+    menuY = rect.top + rect.height;
+
+    if (rect.left + rect.width > this.width - (2 * this.width) / this.gridX) {
+      menuX -= 500; //special case for the first two columns from the right
+    }
+
+    menu.style.left = menuX + "px";
+    menu.style.top = menuY + "px";
+
+    // Update text based on the clicked block's coordinates
+    var divName = div.getAttribute("id");
+    var newText = this.textMappings[divName] || "";
+    var menuText = document.getElementById("menu-text");
+    menuText.textContent = newText;
+
+    if (div.classList.contains("active")) {
+      menu.style.visibility = "visible";
+    } else {
+      menu.style.visibility = "hidden";
+    }
+  };
+
+  this.closeMenu = function () {
+    var self = this;
+    var closeButton = menu.querySelector(".close");
+    closeButton.addEventListener("click", function () {
+      menu.style.visibility = "hidden";
+      if (self.currentActiveDiv) {
+        self.currentActiveDiv.classList.remove("active");
+      }
+    });
+  };
+
+  this.closeMenu();
+
+  this.imageUrls = [
+    "powerpoint1.png",
+    "powerpoint2.png",
+    "powerpoint3.png",
+    "powerpoint4.png",
+    "powerpoint5.png",
+    "powerpoint6.png",
+  ];
+
+ // Function to update background image of active div
+ this.updateActiveDivBackground = function (direction) {
+    var activeDiv = document.querySelector(".active");
+    if (activeDiv) {
+        var currentImageUrl = activeDiv.style.backgroundImage.split('"')[1];
+        var currentIndex = this.imageUrls.indexOf(currentImageUrl);
+        var newIndex;
+
+        if (direction === "next") {
+            newIndex = (currentIndex + 1) % this.imageUrls.length;
+        } else if (direction === "prev") {
+            newIndex = (currentIndex - 1 + this.imageUrls.length) % this.imageUrls.length;
+        }
+
+        activeDiv.style.backgroundImage = "url('" + this.imageUrls[newIndex] + "')";
+    }
+};
+
+// Function to update entire row to the next or previous version
+this.updateRow = function (direction) {
+    var activeDiv = document.querySelector(".active");
+    if (activeDiv) {
+        var currentRow = activeDiv.id.split("_")[1]; // Get row number from div ID
+        var rowDivs = document.querySelectorAll('[id^="div_' + currentRow + '_"]');
+        rowDivs.forEach(function (div) {
+            var currentImageUrl = div.style.backgroundImage.split('"')[1];
+            var currentIndex = loadSplit.imageUrls.indexOf(currentImageUrl); // Use loadSplit to access imageUrls
+            var newIndex;
+
+            if (direction === "next") {
+                newIndex = (currentIndex + 1) % loadSplit.imageUrls.length; // Use loadSplit to access imageUrls
+            } else if (direction === "prev") {
+                newIndex = (currentIndex - 1 + loadSplit.imageUrls.length) % loadSplit.imageUrls.length; // Use loadSplit to access imageUrls
+            }
+
+            div.style.backgroundImage = "url('" + loadSplit.imageUrls[newIndex] + "')"; // Use loadSplit to access imageUrls
+        });
+    }
+};
+
+// Function to update entire column to the next or previous version
+this.updateColumn = function (direction) {
+    var activeDiv = document.querySelector(".active");
+    if (activeDiv) {
+        var currentColumn = activeDiv.id.split("_")[2]; // Get column number from div ID
+        var columnDivs = document.querySelectorAll('[id$="_' + currentColumn + '"]');
+        columnDivs.forEach(function (div) {
+            var currentImageUrl = div.style.backgroundImage.split('"')[1];
+            var currentIndex = loadSplit.imageUrls.indexOf(currentImageUrl); // Use loadSplit to access imageUrls
+            var newIndex;
+
+            if (direction === "next") {
+                newIndex = (currentIndex + 1) % loadSplit.imageUrls.length; // Use loadSplit to access imageUrls
+            } else if (direction === "prev") {
+                newIndex = (currentIndex - 1 + loadSplit.imageUrls.length) % loadSplit.imageUrls.length; // Use loadSplit to access imageUrls
+            }
+
+            div.style.backgroundImage = "url('" + loadSplit.imageUrls[newIndex] + "')"; // Use loadSplit to access imageUrls
+        });
+    }
+};
+
+// Function to update entire canvas to the next or previous version
+this.updateCanvas = function (direction) {
+    var divs = document.querySelectorAll(".split > div");
+    divs.forEach(function (div) {
+        var currentImageUrl = div.style.backgroundImage.split('"')[1];
+        var currentIndex = loadSplit.imageUrls.indexOf(currentImageUrl); // Use loadSplit to access imageUrls
+        var newIndex;
+
+        if (direction === "next") {
+            newIndex = (currentIndex + 1) % loadSplit.imageUrls.length; // Use loadSplit to access imageUrls
+        } else if (direction === "prev") {
+            newIndex = (currentIndex - 1 + loadSplit.imageUrls.length) % loadSplit.imageUrls.length; // Use loadSplit to access imageUrls
+        }
+
+        div.style.backgroundImage = "url('" + loadSplit.imageUrls[newIndex] + "')"; // Use loadSplit to access imageUrls
     });
 };
 
-new Split();
-console.log("tes");
+};
+window.addEventListener("load", function () {
+  loadSplit = new Split();
+  console.log("content loaded");
+});
 
+prevBlockButton.addEventListener("click", function () {
+    loadSplit.updateActiveDivBackground("prev");
+});
 
+prevRowButton.addEventListener("click", function () {
+    loadSplit.updateRow("prev");
+});
+
+prevColumnButton.addEventListener("click", function () {
+    loadSplit.updateColumn("prev");
+});
+
+prevCanvasButton.addEventListener("click", function () {
+    loadSplit.updateCanvas("prev");
+});
+
+nextBlockButton.addEventListener("click", function () {
+    loadSplit.updateActiveDivBackground("next");
+});
+
+nextRowButton.addEventListener("click", function () {
+    loadSplit.updateRow("next");
+});
+
+nextColumnButton.addEventListener("click", function () {
+    loadSplit.updateColumn("next");
+});
+
+nextCanvasButton.addEventListener("click", function () {
+    loadSplit.updateCanvas("next");
+});
 
 //event listener DOMContentloaded = html finishes loading
 //event listener load = everything finishes loading (except lazy, lay is not considered
 
+powerpointButton.addEventListener("click", function () {
+  wordButton.classList.remove("btn-default");
+  excelButton.classList.remove("btn-default");
+  powerpointButton.classList.add("btn-default");
+});
 
-//calculate the position of the div that's clicked on
-//update global variable of tooltip top left position
-//show tooltip
-//toggle class for button
+wordButton.addEventListener("click", function () {
+  powerpointButton.classList.remove("btn-default");
+  excelButton.classList.remove("btn-default");
+  wordButton.classList.add("btn-default");
+});
+
+excelButton.addEventListener("click", function () {
+  powerpointButton.classList.remove("btn-default");
+  wordButton.classList.remove("btn-default");
+  excelButton.classList.add("btn-default");
+});
